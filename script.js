f1e390001f8eb3788ce80f13b0b23eb86b19fd9c
Jacob Counts <infamous213@gmail.com>
2020-11-11 16:58:29 -0700

    added timer/score

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const questionContainer = document.querySelector("#question-container");
 const questionText = document.querySelector("#question-text");
 const answerDiv = document.querySelector("#answers");
 const endPrompt = document.querySelector("#end-prompt");
-const gameOver = document.querySelector(".gameOver") 
+const gameOver = document.querySelector(".gameOver");
 const userScoreEl = document.querySelector("#score");
+const submitHigh = document.querySelector("#submit-high");
+const timeLeftDisplay = document.querySelector("#time");
 
 const questions = [
   {
@@ -15,62 +17,78 @@ const questions = [
   },
   {
     text: "Who was the first American president?",
-    answers: ["Abraham Lincoln", "John Adams", "Andrew Jackson", "George Washington"],
+    answers: [
+      "Abraham Lincoln",
+      "John Adams",
+      "Andrew Jackson",
+      "George Washington",
+    ],
     correctIndex: 3,
   },
-  { 
+  {
     text: "When was the Declaration of Independence signed?",
-    answers: ["July 4th 1776", "July 4th 1786", "July 4th 1906", "July 4th 1766"],
+    answers: [
+      "July 4th 1776",
+      "July 4th 1786",
+      "July 4th 1906",
+      "July 4th 1766",
+    ],
     correctIndex: 0,
   },
-  { 
+  {
     text: "In what state is Mount Rushmore located?",
     answers: ["Noth Dakota", "Iowa", "South Dakota", "Montana"],
     correctIndex: 2,
   },
-  { 
+  {
     text: "Which president is on the $50 bill?",
     answers: ["Franklin", "Grant", "Jefferson", "Jackson"],
     correctIndex: 1,
   },
-  { 
+  {
     text: "What state is the Liberty Bell located?",
     answers: ["Pennsylvania", "Washington DC", "California", "Florida"],
     correctIndex: 0,
   },
-  { 
+  {
     text: "What is the longest river in the USA?",
     answers: ["Mississippi", "Colorado", "Yukon", "Missouri"],
     correctIndex: 3,
   },
-  { 
+  {
     text: "Where is the Statue of Liberty located?",
     answers: ["New York", "New Jersey", "Washington DC", "Pennsylvania"],
     correctIndex: 0,
   },
-  { 
+  {
     text: "What year was the Boston Tea Party?",
     answers: ["1873", "1809", "1773", "1776"],
     correctIndex: 2,
   },
-  { 
+  {
     text: "Who created the first American flag ?",
     answers: ["Rick Ross", "George Washington", "Bob Ross", "Betsy Ross"],
     correctIndex: 3,
   },
- 
-  
-
 ];
-
 
 let questionIndex = 0;
 
 // registering a click event handler
 startBtn.addEventListener("click", handleStartClick);
 answerDiv.addEventListener("click", handleAnswerClick);
+let timer;
+function countDown() {
+  timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      timeLeftDisplay.innerHTML = 0;
 
-
+      endScreen();
+    }
+    timeLeftDisplay.innerHTML = timeLeft;
+    timeLeft -= 1;
+  }, 1000);
+}
 function handleAnswerClick(e) {
   e.preventDefault();
 
@@ -86,13 +104,13 @@ function handleAnswerClick(e) {
   // compare to the answer the user selected
   if (userAnswer === correctAnswer) {
     // if they answered correctly move onto the next question
-    
+    score += 5;
   } else {
+    timeLeft -= 10;
+
     // else remove 10 seconds from the time move onto next question
   }
-      
 
-    
   questionIndex++;
   // do we even have anymore question to render?
   // if not end the game
@@ -105,7 +123,6 @@ function handleAnswerClick(e) {
     //console.log("more questions")
     renderQuestion();
   }
-
 }
 
 function handleStartClick(e) {
@@ -117,7 +134,6 @@ function handleStartClick(e) {
 
   renderQuestion();
 }
-
 
 function renderQuestion() {
   // create a variable to store our current question
@@ -141,48 +157,44 @@ function renderQuestion() {
     answerDiv.appendChild(btn);
   }
 }
+let timeLeft = 30;
 // countDown timer
-document.addEventListener("DOMContentLoaded",  function() {
-    const timeLeftDisplay = document.querySelector("#time")
-    const startBtn = document.querySelector("#start")
-    let timeLeft = 30
+// document.addEventListener("DOMContentLoaded",  function() {
 
-    
-    function countDown(){
-        setInterval(function() {
-            if(timeLeft <= 0 ) {
-                clearInterval(timeLeft =0)
-            }
-            timeLeftDisplay.innerHTML = timeLeft
-            timeLeft -=1
-        }, 1000)
-    }
-startBtn.addEventListener("click", countDown)
+// })
 
-})
-
-// end of game 
-function endScreen () {
+// end of game
+function endScreen() {
   questionContainer.style.display = "none";
   gameOver.style.display = "block";
   time.style.display = "none";
-
+  clearInterval(timer);
+  document.querySelector("#score-btn").textContent = timeLeft;
 }
 
+var score = 0;
+var currentQ = 0;
+startBtn.addEventListener("click", countDown);
+submitHigh.addEventListener("click", submitHighScore);
 
+function submitHighScore() {
+  gameOver.style.display = "none";
+  document.querySelector("#highScores").style.display = "block";
+  var initials = document.querySelector("#final").value;
+  var score = document.querySelector("#score-btn").textContent;
 
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  highScores.push({ score, initials });
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+  highScores = highScores.sort((a, b) => b.score - a.score);
+  var list = document.querySelector("#highScoreList");
 
-//var score = 0;
-//var currentQ = 0;
+  for (let i = 0; i < highScores.length; i++) {
+    const s = highScores[i];
 
-//function scoreKeeper() {
-  //var answer = correctIndex.id-1;
-  //if (questions[currentQ].correctIndex[i] === questions[currentQ].correctIndex[answer]) {
-    //score += 5;
-  //}else {
-    //timeLeft += 10;
-  //}
-  //};
-
-
-
+    var liel = document.createElement("li");
+    liel.textContent = s.initials + ":" + s.score;
+    list.appendChild(liel);
+  }
+}
